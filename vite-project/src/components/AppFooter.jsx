009f707a23752e912a20720426ecc0fbf3b0a7dc
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-12-12 17:37:33 +0100

    feat: finalizzazione consegna con bonus

--- vite-project/src/components/AppFooter.jsx
+++ vite-project/src/components/AppFooter.jsx
@@ -1,7 +1,13 @@
+import style from "./AppFooter.module.css";
+
 function AppFooter () {
 
     return (
-        <div>footer</div>
+        <footer className={style.footer}>
+            <div className="container">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sapiente pariatur velit libero commodi quis. Reiciendis dolorum fugit sequi atque a, perspiciatis voluptates commodi praesentium, neque explicabo repellat itaque. Est!
+            </div>
+        </footer>
     )
 }
 export default AppFooter;
--- vite-project/src/components/AppMain.jsx
+++ vite-project/src/components/AppMain.jsx
@@ -1,9 +1,12 @@
+import style from "./AppMain.module.css";
+import PostCard from "./PostCard";
+
 function AppMain() {
 
     return (
         <div className="container">
-            <section>
-
+            <section className={style.text}>
+                <PostCard />
             </section>
 
         </div>
